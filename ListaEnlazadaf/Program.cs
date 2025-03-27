345d908c865dbc2983120f0fd7d364abb56d2699
jordan9878 <jordan_78_8@hotmail.com>
2025-03-27 17:43:20 -0500

    vagones

--- ListaEnlazadaf/Program.cs
+++ ListaEnlazadaf/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ListaEnlazadaf;
+
+locomotor l = new locomotor();
+l.AgregaFinal(2);
+l.AgregaFinal(10);
+l.AgregaFinal(20);
+Console.WriteLine(l.VerVagones());
---
+++ ListaEnlazadaf/locomotor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaEnlazadaf
+{
+    class locomotor
+    {
+        vagon Primero { get; set; }
+        public void AgregaPrimero(int valor)
+        {
+            vagon nuevo = new vagon(valor);
+            if (this.Primero == null)
+            {
+                this.Primero = nuevo;
+            }
+            else
+            {
+                nuevo.Sig = this.Primero;
+                vagon tmp = this.Primero;
+                while (tmp.Sig != null)
+                {
+                    tmp = tmp.Sig;
+                }
+                tmp.Sig = nuevo;
+            }
+        }
+            public void AgregaFinal(int valor)
+        {
+            vagon nuevo = new vagon(valor);
+            if(this.Primero == null)
+            {
+                this.Primero = nuevo;
+            }
+            else
+            {
+                vagon tmp = this.Primero;
+                while (tmp.Sig != null)
+                {
+                    tmp = tmp.Sig;
+                }
+                tmp.Sig = nuevo;
+            }
+        }
+            public string VerVagones()
+        {
+            string listaVag = "";
+            vagon tmp = this.Primero;
+            while (tmp != null)
+            {
+                listaVag += tmp.Dato + ", ";
+                tmp = tmp.Sig;
+            }
+
+            return listaVag;
+        }
+    }
+}
